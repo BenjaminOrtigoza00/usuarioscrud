2b89c7d8fc966f6d03e3e8aad1995647b8592b51
Benjamin Ortigoza <ortigozabenjamin100@gmail.com>
2025-04-22 01:09:43 -0300

    Esquema de Mongoose para usuarios

--- usuarios.model.ts
+++ usuarios.model.ts
@@ -1,1 +1,14 @@
 //definicion de campos y validacion de los uruarios
+
+import mongoose from "mongoose";
+
+// Definimos la estructura de los documentos "usuarios"
+const UsuarioSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  edad: { type: Number, required: true },
+  nacionalidad: { type: String, required: true },
+  rol: { type: String, required: true }, // "admin", "cliente", etc.
+});
+
+// Creamos y exportamosgit
+export const Usuario = mongoose.model("Usuario", UsuarioSchema);
