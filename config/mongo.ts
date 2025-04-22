e494e4516a43be88d466af41d2d8dc368b903ffc
Benjamin Ortigoza <ortigozabenjamin100@gmail.com>
2025-04-22 00:03:32 -0300

    coneccion a mongoDB

--- config/mongo.ts
+++ config/mongo.ts
@@ -1,1 +1,9 @@
 //conexion base de datos mongoDB: pendiente
+import mongoose, { connect } from "mongoose";
+
+const conectDB = async () => {
+    await mongoose.connect("mongodb://localhost:27017/");
+    console.log("conectado a mongo");
+};
+
+export { conectDB };
